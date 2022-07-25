5c2f9a398dfc96f53f19aa84ec157bd9f57cca95
Udit Gandhi <coder.udit@gmail.com>
2022-07-25 18:59:26 -0300

    Updated with initial code of Catalog API

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,31 @@
+﻿using CatalogAPI.Models;
+using CatalogAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        //GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return repository.GetItems();
+        }
+
+        //GET /items/id
+        [HttpGet("{id}")]
+        public Item? GetItem(Guid id) {
+            return repository.GetItem(id);
+        }
+    }
+}
